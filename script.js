96d46e5e60607f2bc243ad28cd648b801fc5ee24
n-avontuur <55791419+n-avontuur@users.noreply.github.com>
2022-08-03 23:58:43 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const RPC = require('discord-rpc');
+const client = new RPC.Client({ transport: 'ipc' });
+client.on('ready', () =>
+{
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity: {
+            assets: {
+                large_image: 'woc', //WoC
+                small_image: 'pycord_logo', //Pycord
+                small_text: 'Pycord!',
+                large_text: "World of Coding!",
+            },
+
+            details: '11k+ CODING SERVER/REDDIT!', 
+            buttons: [
+                { label: 'Join World of Coding!', url: 'https://discord.gg/pkShK9ax2X' }, //woc (Partner of Pycord)
+                { label: 'Join r/code!', url: 'https://www.reddit.com/r/code'}, //r/code (Reddit page)
+            ]
+        }
+    });
+    console.log('started!');
+});
+client.login({ clientId: '803978215466074122' });
